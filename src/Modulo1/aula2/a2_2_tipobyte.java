60171b3a795f67be956d6cb75a3241caf533caa0
Aline Ribeiro <aline.ribeiro@capgemini.com>
2021-09-10 15:34:05 -0300

    Adiciona classe main

---
+++ src/Modulo1/aula2/a2_2_tipobyte.java
@@ -0,0 +1,18 @@
+package Modulo1.aula2;
+
+public class a2_2_tipobyte {
+
+   public static void main(String args[]){
+
+       System.out.println(soma(10,15));
+
+   }
+   private static int soma(int n1, int n2){
+       int result = n1 + n2;
+       return result;
+
+    }
+    int soma() {
+       return 10;
+    }
+}
