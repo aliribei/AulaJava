ea38159b62e8431599ad273379a7dc27a721b0a0
Aline Ribeiro <aline.ribeiro@capgemini.com>
2021-09-13 16:15:59 -0300

    Adiciona descricoa e valor em classe de produtos

--- src/Atividades/Atividades1/Produto.java
+++ src/Atividades/Atividades1/Produto.java
@@ -2,7 +2,18 @@ package Atividades.Atividades1;
 
 public class Produto {
     private int codigoProduto;
+    private float valor;
     private String categoria;
+    private String nome;
+    private String descricao;
+
+    public void setValor(float valor){
+        this.valor = valor;
+    }
+
+    public float getValor(){
+        return valor;
+    }
 
     public void setCodigoProduto(int codigoProduto){
         this.codigoProduto = codigoProduto;
@@ -18,5 +29,21 @@ public class Produto {
 
     public String getCategoria(){
         return categoria;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
     }
 }
--- src/Atividades/Atividades1/Teste.java
+++ src/Atividades/Atividades1/Teste.java
@@ -1,4 +1,6 @@
 package Atividades.Atividades1;
+import java.util.Locale;
+import java.text.NumberFormat;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -10,27 +12,42 @@ public class Teste {
         Scanner leitor = new Scanner(System.in);
         Produto produto = new Produto();
 
+        System.out.print("Nome do produto: ");
+        produto.setNome(leitor.nextLine());
+
         System.out.print("Código do produto: ");
         produto.setCodigoProduto(leitor.nextInt());
+
+        System.out.print("Valor do produto: ");
+        produto.setValor(leitor.nextFloat());
 
         System.out.print("Categoria do produto: ");
         leitor.nextLine();// limpa o scanner
         produto.setCategoria(leitor.nextLine());
 
+        System.out.print("Descrição do produto: ");
+        produto.setDescricao(leitor.nextLine());
+
+        leitor.nextLine();// limpa o scanner
         produtos.add(produto);
     }
 
     private void imprimirProdutos() {
+        Locale localeBR = new Locale("pt","BR");
+        NumberFormat dinheiro = NumberFormat.getCurrencyInstance(localeBR);
         for(Produto produto : produtos){
-            System.out.println("\nCódigo: " + produto.getCodigoProduto());
-            System.out.println("Categoria: " + produto.getCategoria());
+            System.out.println("Lista de produtos cadastrados");
+            System.out.println("Nome:   " + produto.getNome().toUpperCase());
+            System.out.println("Código: " + produto.getCodigoProduto());
+            System.out.println("Valor:  " + dinheiro.format(produto.getValor()));
+            System.out.println("Categoria: " + produto.getCategoria().toUpperCase());
+            System.out.println("Descrição: " + produto.getDescricao().toUpperCase());
             System.out.println("\n\n");
         }
     }
 
-
     public static void main(String[] args){
-        short opcao = 50;
+        short opcao = 3;
         Scanner leitor = new Scanner(System.in);
         Teste teste = new Teste();
 
@@ -43,13 +60,14 @@ public class Teste {
             switch(opcao){
                 case 1:
                     teste.cadastrarProduto();
+                    System.out.println("Produto Cadastrado com sucesso");
+                    break;
                 case 2:
                     teste.imprimirProdutos();
-                case 50:
+                    break;
+                case 3:
                     teste.exibirMenu();
                     break;
-                default:
-                    teste.exibirMenu();
             }
         }while(opcao != 0);
     }
@@ -57,11 +75,12 @@ public class Teste {
     private void exibirMenu() {
         System.out.println("\n\n");
         System.out.println("+-------------------------------------------+");
-        System.out.println("|        Menu de Opções                     |");
+        System.out.println("|        Cadastro de produtos               |");
+        System.out.println("|          Escolha uma opção:               |");
         System.out.println("+-------------------------------------------+");
         System.out.println("| 1 - Cadastrar Produto                    |");
         System.out.println("| 2 - Imprimir relatório de produtos       |");
-        System.out.println("| 50 - Exibir o menu de opções              |");
+        System.out.println("| 3 - Exibir o menu de opções              |");
         System.out.println("| 0 - Sair                                 |");
         System.out.println("+-------------------------------------------+");
     }
